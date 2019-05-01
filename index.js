8b25f55b2387e15f3db2afd7b414cc455e3efd59
Anh DH <anhdhbn@gmail.com>
2019-05-01 17:18:39 +0700

    update new version of flowchart  and raphael
    flowchart 1.6.5->1.11.3
    raphael 2.2.7->2.2.8

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var assign = require('deep-assign');
 var renderer = require('./lib/renderer');
 
 hexo.config.flowchart = assign({
-  flowchart: 'https://cdnjs.cloudflare.com/ajax/libs/flowchart/1.6.5/flowchart.min.js',
-  raphael: 'https://cdnjs.cloudflare.com/ajax/libs/raphael/2.2.7/raphael.min.js',
+  flowchart: 'https://cdnjs.cloudflare.com/ajax/libs/flowchart/1.11.3/flowchart.min.js',
+  raphael: 'https://cdnjs.cloudflare.com/ajax/libs/raphael/2.2.8/raphael.min.js',
   options: {
     'scale': 1,
     'line-width': 2,
